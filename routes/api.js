953457fb7f9acefdcd30229a4e557e8ceff546aa
josamaci <josamaci@espol.edu.ec>
2022-08-15 22:28:44 -0500

    arreglando xd

--- routes/api.js
+++ routes/api.js
@@ -2,25 +2,31 @@ var express = require('express');
 var router = express.Router();
 const { Sequelize, Op } = require('sequelize');
 const Producto = require('../models').producto;
-const Api = require('../models').api;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-    router.get('/productos', function(req, res, next) {
-        res.render('productos', { title: 'Productos' });
-    })
+    res.send('respond with a resource');
+
 });
 
 router.get('/productos', function(req, res, next) {
-
     Producto.findAll({
             attributes: { exclude: ["updatedAt"] }
         })
         .then(productos => {
-            res.render('productos', { title: 'My Dashboard :: Productos', arrProductos: productos });
+            res.json(productos)
         })
         .catch(error => res.status(400).send(error))
+});
 
+router.get('/productos/:id', function(req, res, next) {
+    Producto.findOne({
+            where: { id: req.params.id }
+        })
+        .then(productos => {
+            res.json(productos)
+        })
+        .catch(error => res.status(400).send(error))
 });
 
 module.exports = router;
