3fe58802f07524fafdbbd43ef8ebdfaa35918a36
josamaci <josamaci@espol.edu.ec>
2022-08-17 14:08:13 -0500

    xd

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const { Sequelize, Op } = require('sequelize');
+const Sequelize = require('sequelize');
 const Producto = require('../models').producto;
-const Api = require('../models').api;
+var arrProductos;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
-    router.get('/productos', function(req, res, next) {
-        res.render('productos', { title: 'Productos' });
-    })
 });
 
 router.get('/productos', function(req, res, next) {
-
     Producto.findAll({
             attributes: { exclude: ["updatedAt"] }
         })
@@ -20,6 +17,19 @@ router.get('/productos', function(req, res, next) {
             res.render('productos', { title: 'My Dashboard :: Productos', arrProductos: productos });
         })
         .catch(error => res.status(400).send(error))
+        //res.render('productos', { title: 'Productos' });
+
+});
+
+router.get('/reporte', function(req, res, next) {
+    Producto.findAll({
+            attributes: { exclude: ["updatedAt"] }
+        })
+        .then(productos => {
+            res.render('reporte', { title: 'My Dashboard :: Reporte', arrProductos: productos });
+        })
+        .catch(error => res.status(400).send(error))
+        //res.render('productos', { title: 'Productos' });
 
 });
 
